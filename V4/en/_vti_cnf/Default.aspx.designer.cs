b7a933617b5ac6613aa184281b106931127e8623
Ygor Lazaro <ygor@ygorlazaro.com>
2015-10-05 01:08:06 -0300

    Limpeza

--- V4/en/_vti_cnf/Default.aspx.designer.cs
+++
@@ -1,6 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|06 Jun 2010 02:08:20 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_cacheddtm:TX|06 Jun 2010 02:08:20 -0000
-vti_filesize:IR|446
-vti_backlinkinfo:VX|
--- V4/js/_vti_cnf/prototype.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|08 Apr 2010 19:58:35 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|en/MasterPageEn.Master MasterPage.master
-vti_author:SR|PYSI\\Ygor Lazaro
-vti_modifiedby:SR|PYSI\\Ygor Lazaro
-vti_timecreated:TR|08 Apr 2010 19:58:35 -0000
-vti_cacheddtm:TX|08 Apr 2010 19:58:35 -0000
-vti_filesize:IR|149849
